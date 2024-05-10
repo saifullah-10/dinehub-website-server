9e2e7ef5e8bf67ac04877bd972fd3386b2b6cfbe
saifullah-10 <saifullah.bus@gmail.com>
2024-05-10 22:22:27 +0600

    add a method for homepage data

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cookieParser = require("cookie-parser");
+require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -8,6 +10,47 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+// mongoDB
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.kpsyb7k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+const database = client.db("restaurent");
+const restaurent = database.collection("all_foods");
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+    app.get("/homecard", async (req, res) => {
+      const elementAmount = req.query.elements;
+      const cursor = await restaurent
+        .find()
+        .limit(parseInt(elementAmount))
+        .toArray();
+      res.send(cursor);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", async (req, res) => {
   res.send("server is running");
